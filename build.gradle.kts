b063f9b312fbfb9df524e092d6da0d0c101196ec
Egor Kukoverov <me@bnt.to>
2023-05-11 13:28:06 +0300

    initialize plugin

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+    id("org.jetbrains.intellij") version "1.13.3"
+}
+
+group = "to.bnt.plugin"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2022.2.5")
+    type.set("IC")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        version.set("${project.version}")
+        sinceBuild.set("222")
+        untilBuild.set("232.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
